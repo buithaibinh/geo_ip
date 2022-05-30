a0563819dddb89d5347c8c2db4da102aae10ea88
Binh Bui <binhbv@gmail.com>
2022-05-30 11:57:05 +0700

    myip

--- pages/api/myip.ts
+++ pages/api/myip.ts
@@ -7,9 +7,9 @@ interface GeoIPResponse extends Lookup {}
 
 type Data = {
   data?: {
-    ip: string,
-    geo?: GeoIPResponse,
-  }
+    ip: string | null | [string];
+    geo: GeoIPResponse | null;
+  };
   error?: string;
 };
 
@@ -17,8 +17,14 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
- // get client ip
+  // get client ip
   let ip: any = req.headers['x-real-ip'];
-  console.log(req.headers);
-  return res.status(200).json({ data: { ip } });
+
+  if (req.query.ip) {
+    ip = req.query.ip;
+  }
+
+  const lookup = geo.lookup(ip);
+
+  return res.status(200).json({ data: { ip, geo: lookup } });
 }
